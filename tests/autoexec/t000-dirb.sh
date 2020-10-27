bd0d65272f4b27496eba2ade59099eef68336aa6
Patryk Obara <dreamer.tan@gmail.com>
2020-10-27 17:46:46 +0100

    WIP Test dummy videodriver test
    Perhaps this technique is good enough to replace xvfb-run we currently
    use in sanitizers.
    
    Try to run on CI on Linux and macOS

---
+++ tests/autoexec/t000-dirb.sh
@@ -0,0 +1,25 @@
+#!/bin/env bash
+
+export SDL_VIDEODRIVER=dummy
+readonly DOSBOX="$(git rev-parse --show-toplevel)/src/dosbox"
+
+expecting_output () {
+	echo "$1" > t000-dirb-out.txt
+	unix2dos t000-dirb-out.txt
+}
+
+set -xe
+
+rm -rfv drive_c/*
+mkdir drive_c
+
+$DOSBOX -conf t000-dirb.conf
+
+expecting_output \
+'HELLO.TXT
+OUT.TXT'
+
+diff -u t000-dirb-out.txt drive_c/OUT.TXT
+
+rm -rfv drive_c
+rm t000-dirb-out.txt
